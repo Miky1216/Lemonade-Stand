63f47de7769a1842fa9fd8c1b7016f1d162cb25b
Miky1216 <michael.s.terrill@gmail.com>
2015-07-30 13:56:02 -0500

    Initial committ stubbed code

---
+++ LemonadeStand.py
@@ -0,0 +1,33 @@
+import random
+
+class TimeofDay:
+    def CheckTimeOfDay():
+        pass
+class User:    
+    def __init__(self, Participants):
+        self.name
+        self.demand
+
+class UserInput:
+    def GetUserName():
+        pass
+class InitialCalculations:
+    def ChangeInForecast():
+        pass
+    def ChangeInOutsideTemp():
+        pass
+    def CalculatePitcherCupPrice():
+        pass
+
+class ProfitCalculations:
+    def CalculateProfit():
+        pass
+    def ShowProfitLoss():
+        pass
+
+class GameLoop:
+    def RunGame():
+    
+
+    
+GameLoop.RunGame()
